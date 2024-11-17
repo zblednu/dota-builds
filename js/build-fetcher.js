c6c9b3d559a79a849dc2d92953bf45343dbbd05c
zblednu <aguy>
2024-11-17 14:09:43 +0100

    first version

---
+++ js/build-fetcher.js
@@ -0,0 +1,3 @@
+export default function getBuild(text) {
+  return `i'm a bot and i'm here to do some magic with: ${text} `;
+}
---
+++ js/index.js
@@ -0,0 +1,28 @@
+import getBuild from './build-fetcher.js';
+const input = document.querySelector('input');
+input.focus();
+const chatWindow = document.querySelector('.chat-window');
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === 'Enter' && input.value) {
+    addChatMessage(input.value, true);
+    addChatMessage(getBuild(input.value), false);
+
+    chatWindow.scroll({
+      top: chatWindow.scrollHeight,
+      behavior: "smooth"
+    });
+    input.value = "";
+    input.focus();
+  }
+})
+
+// user = true for user's message, user = false for bot message
+function addChatMessage(text, user) {
+  const comment = document.createElement('div');
+  comment.className = `comment ${user ? 'user' : 'bot'}-response`;
+  comment.textContent = text;
+
+  chatWindow.appendChild(comment);
+}
+
